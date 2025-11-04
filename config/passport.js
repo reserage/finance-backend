c68f97de2a12ea181d1060e7635764c4533b46b5
reserage <aa0938790857@gmail.com>
2025-11-05 00:14:44 +0800

    .

--- config/passport.js
+++ config/passport.js
@@ -1,15 +1,15 @@
-require("dotenv").config(); // 這行程式碼會讀取.env檔案中的環境變數
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20");
-const User = require("../models/user");
-const LocalStrategy = require("passport-local");
-const bcrypt = require("bcrypt");
-const Category = require("../models/category");
-const BookKeeping = require("../models/bookKeeping");
+require('dotenv').config(); // 這行程式碼會讀取.env檔案中的環境變數
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+const User = require('../models/user');
+const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
+const Category = require('../models/category');
+const BookKeeping = require('../models/bookKeeping');
 const defaultItem = require('../utils/defaultItem');
 
 passport.serializeUser((user, done) => {
-  console.log("Serialize使用者");
+  console.log('Serialize使用者');
   // console.log("serializeUser使用者的id: ", user._id);
   done(null, user._id); // 將mongoDB的id，存在session內
   // 並且將id簽名後，以Cookie的形式給使用者
@@ -17,7 +17,7 @@ passport.serializeUser((user, done) => {
 
 passport.deserializeUser(async (_id, done) => {
   console.log(
-    "Deserialize使用者。。。使用serializeIser儲存的id，去找到資料庫內的資料"
+    'Deserialize使用者。。。使用serializeIser儲存的id，去找到資料庫內的資料'
   );
   let foundUser = await User.findOne({ _id }).exec();
   // console.log("deserializeUser使用者的資料: ", foundUser);
@@ -32,10 +32,10 @@ passport.use(
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL:
         process.env.GOOGLE_CALLBACK_URL ||
-        "http://localhost:5000/auth/google/redirect", // 如果沒有設定環境變數，則使用預設值
+        'http://localhost:5000/auth/google/redirect', // 如果沒有設定環境變數，則使用預設值
     },
     async (accessToken, refreshToken, profile, done) => {
-      console.log("進入Google Strategy的區域");
+      console.log('進入Google Strategy的區域');
       // console.log("profile: ",profile);
       // console.log("==========================");
       let foundUser = await User.findOne({ googleId: profile.id }).exec();
@@ -43,15 +43,15 @@ passport.use(
         email: profile.emails[0].value,
       });
       if (foundUser) {
-        console.log("使用者已經註冊過了，無須存入資料庫內");
+        console.log('使用者已經註冊過了，無須存入資料庫內');
         console.log(foundUser);
         done(null, foundUser);
       } else if (foundEmail) {
-        console.log("使用者的email已經經過本地註冊過了，無須存入資料庫內");
+        console.log('使用者的email已經經過本地註冊過了，無須存入資料庫內');
         console.log(foundEmail);
         done(null, foundEmail);
       } else {
-        console.log("偵測到新用戶，須將資料存入資料庫內");
+        console.log('偵測到新用戶，須將資料存入資料庫內');
         let newUser = new User({
           name: profile.displayName,
           googleId: profile.id,
@@ -61,8 +61,8 @@ passport.use(
         let savedUser = await newUser.save();
 
         // --------------------將預設的類別存入資料庫--------------------
-        let defaultIncomeCategory = ["薪水", "投資"];
-        let defaultExpenseCategory = ["餐飲", "交通", "娛樂", "其他"];
+        let defaultIncomeCategory = ['薪水', '投資'];
+        let defaultExpenseCategory = ['餐飲', '交通', '娛樂', '其他'];
 
         for (const income of defaultIncomeCategory.reverse()) {
           const category = new Category({
@@ -86,52 +86,52 @@ passport.use(
         const years = [2024, 2025]; // 用於設定有預設記帳本的年齡
         const bookInfo = [
           {
-            name: "1月記帳本",
-            description: "這是一個範例視圖，您可以在此處添加任何內容。",
+            name: '1月記帳本',
+            description: '這是一個範例視圖，您可以在此處添加任何內容。',
           },
           {
-            name: "2月記帳本",
-            description: "這是一個範例視圖，您可以在此處添加任何內容。",
+            name: '2月記帳本',
+            description: '這是一個範例視圖，您可以在此處添加任何內容。',
           },
           {
-            name: "3月記帳本",
-            description: "這是一個範例視圖，您可以在此處添加任何內容。",
+            name: '3月記帳本',
+            description: '這是一個範例視圖，您可以在此處添加任何內容。',
           },
           {
-            name: "4月記帳本",
-            description: "這是一個範例視圖，您可以在此處添加任何內容。",
+            name: '4月記帳本',
+            description: '這是一個範例視圖，您可以在此處添加任何內容。',
           },
           {
-            name: "5月記帳本",
-            description: "這是一個範例視圖，您可以在此處添加任何內容。",
+            name: '5月記帳本',
+            description: '這是一個範例視圖，您可以在此處添加任何內容。',
           },
           {
-            name: "6月記帳本",
-            description: "這是一個範例視圖，您可以在此處添加任何內容。",
+            name: '6月記帳本',
+            description: '這是一個範例視圖，您可以在此處添加任何內容。',
           },
           {
-            name: "7月記帳本",
-            description: "這是一個範例視圖，您可以在此處添加任何內容。",
+            name: '7月記帳本',
+            description: '這是一個範例視圖，您可以在此處添加任何內容。',
           },
           {
-            name: "8月記帳本",
-            description: "這是一個範例視圖，您可以在此處添加任何內容。",
+            name: '8月記帳本',
+            description: '這是一個範例視圖，您可以在此處添加任何內容。',
           },
           {
-            name: "9月記帳本",
-            description: "這是一個範例視圖，您可以在此處添加任何內容。",
+            name: '9月記帳本',
+            description: '這是一個範例視圖，您可以在此處添加任何內容。',
           },
           {
-            name: "10月記帳本",
-            description: "這是一個範例視圖，您可以在此處添加任何內容。",
+            name: '10月記帳本',
+            description: '這是一個範例視圖，您可以在此處添加任何內容。',
           },
           {
-            name: "11月記帳本",
-            description: "這是一個範例視圖，您可以在此處添加任何內容。",
+            name: '11月記帳本',
+            description: '這是一個範例視圖，您可以在此處添加任何內容。',
           },
           {
-            name: "12月記帳本",
-            description: "這是一個範例視圖，您可以在此處添加任何內容。",
+            name: '12月記帳本',
+            description: '這是一個範例視圖，您可以在此處添加任何內容。',
           },
         ];
 
@@ -142,7 +142,7 @@ passport.use(
             try {
               const newBook = new BookKeeping({
                 name: book.name,
-                date: new Date(`${year}-${String(i + 1).padStart(2, "0")}-01`),
+                date: new Date(`${year}-${String(i + 1).padStart(2, '0')}-01`),
                 description: book.description,
                 user: savedUser._id,
                 isDefault: true,
@@ -159,7 +159,7 @@ passport.use(
 
         defaultItem.getDefaultCities(savedUser._id);
 
-        console.log("成功創建用戶。");
+        console.log('成功創建用戶。');
         done(null, savedUser);
       }
     }
@@ -170,23 +170,26 @@ passport.use(
 passport.use(
   new LocalStrategy(
     {
-      usernameField: "email",
+      usernameField: 'email',
     },
     async (email, password, done) => {
       let foundUser = await User.findOne({ email }).exec();
       console.log(foundUser);
       if (foundUser) {
+        if (!foundUser.password)
+          return done(null, false, { message: '該email已用於google登入' });
+
         let result = await bcrypt.compare(password, foundUser.password);
         if (result) {
-          console.log("登入成功");
-          done(null, foundUser);
+          console.log('登入成功');
+          return done(null, foundUser);
         } else {
-          console.log("密碼錯誤，登入失敗");
-          done(null, false, { message: "密碼錯誤" });
+          console.log('密碼錯誤，登入失敗');
+          return done(null, false, { message: '密碼錯誤' });
         }
       } else {
-        console.log("使用者不存在，登入失敗");
-        done(null, false, { message: "使用者不存在" });
+        console.log('使用者不存在，登入失敗');
+        return done(null, false, { message: '使用者不存在' });
       }
     }
   )
